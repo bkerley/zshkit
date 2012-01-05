7f179385ba890c8a5fc7695cfe7ad4dadf44f643
Bryce Kerley <bkerley@brycekerley.net>
2012-01-05 16:55:39 -0500

    new rails shortcuts

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -1,1 +1,2 @@
 alias irb='irb --readline -r irb/completion'
+function be() { [ "$1" = "a" ] && shift; bundle exec "$@"; }
--- 06_rails.zsh
+++ 06_rails.zsh
@@ -1,2 +1,5 @@
-alias sc='./script/console'
+# alias sc='./script/console'
 alias rr='touch tmp/restart.txt'
+alias ra='rake'
+alias bes="bundle exec $SHELL"
+alias br='bundle exec rake'
