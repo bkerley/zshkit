bc4c457353f8f2d616c8f597d604fab3ca03486b
Will Oberndorfer <will.oberndorfer@gmail.com>
2013-02-07 00:00:18 -0800

    use preview.app for manpage formatting - don't really feel like installing ps2pdf

--- 06_man.zsh
+++ 06_man.zsh
@@ -1,7 +1,6 @@
 # Open manpage with Preview.app
-# Uses pstopdf conversion because it's faster
 if [[ $OSTYPE[1,6] == "darwin" ]]; then
 	function manp () {
-		man -t $* | pstopdf - - | open -f -a Preview
+		man -t $* | open -f -a Preview
 	}
 fi
