d7f720668e898cf4f32e1889cbda36b87e822ee1
Bryce Kerley <bkerley@brycekerley.net>
2013-06-27 19:26:20 -0400

    move usr local bin sooner

--- 01_path.zsh
+++ 01_path.zsh
@@ -1,4 +1,4 @@
-path=($HOME/.rbenv/bin /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin $path /usr/local/mysql/bin/ /opt/git/bin /usr/local/bin  /usr/texbin /usr/sfw/bin /usr/ccs/bin /usr/sbin)
+path=($HOME/.rbenv/bin /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin /usr/local/bin $path /usr/local/mysql/bin/ /opt/git/bin  /usr/texbin /usr/sbin /usr/local/share/npm/bin)
 
 # Allow MacPorts man pages
 manpath=($manpath /opt/local/man)
