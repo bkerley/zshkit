286cbd1b6f51a7474c354cba04d15daf5b57399e
Bryce Kerley <bkerley@brycekerley.net>
2013-07-18 14:51:01 -0400

    Convenient Console Command

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -1,2 +1,7 @@
 alias irb='irb --readline -r irb/completion'
 function be() { [ "$1" = "a" ] && shift; bundle exec "$@"; }
+
+function ccc() {
+  file=$(find lib -type f -depth 1 | head -n1)
+  irb -Ilib -r./$file
+}
