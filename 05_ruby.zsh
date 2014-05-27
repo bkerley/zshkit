f22e2b298bd7cbad0be47a4997b967a94787ca30
Bryce Kerley <bkerley@brycekerley.net>
2014-05-27 19:45:28 -0400

    allow an to work with bundle exec

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -1,5 +1,5 @@
 alias irb='irb --readline -r irb/completion'
-function be() { [ "$1" = "a" ] && shift; bundle exec "$@"; }
+function be() { [[ ("$1" = "a") || ("$1" = "an") ]] && shift; bundle exec "$@"; }
 
 function ccc() {
   file=$(find lib -type f -depth 1 | head -n1)
