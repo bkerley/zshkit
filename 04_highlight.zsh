d40c1b35767d9c4d996016faccfa869750df0290
Bryce Kerley <bkerley@brycekerley.net>
2014-10-20 22:25:08 -0400

    conditionalize syntax highlighting

--- 04_highlight.zsh
+++ 04_highlight.zsh
@@ -1,21 +1,23 @@
-source $HOME/zshkit/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -f $HOME/zshkit/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+    source $HOME/zshkit/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# look at me i can be twlight-mode too
-command_highlight_style="fg=white,bold"
-quote_highlight_style="fg=green"
-option_highlight_style="fg=cyan,bold"
+    # look at me i can be twlight-mode too
+    command_highlight_style="fg=white,bold"
+    quote_highlight_style="fg=green"
+    option_highlight_style="fg=cyan,bold"
 
-ZSH_HIGHLIGHT_STYLES[history-expansion]="fg=yellow"
+    ZSH_HIGHLIGHT_STYLES[history-expansion]="fg=yellow"
 
-ZSH_HIGHLIGHT_STYLES[alias]=$command_highlight_style
-ZSH_HIGHLIGHT_STYLES[builtin]=$command_highlight_style
-ZSH_HIGHLIGHT_STYLES[function]=$command_highlight_style
-ZSH_HIGHLIGHT_STYLES[command]=$command_highlight_style
-ZSH_HIGHLIGHT_STYLES[hashed-command]=$command_highlight_style
+    ZSH_HIGHLIGHT_STYLES[alias]=$command_highlight_style
+    ZSH_HIGHLIGHT_STYLES[builtin]=$command_highlight_style
+    ZSH_HIGHLIGHT_STYLES[function]=$command_highlight_style
+    ZSH_HIGHLIGHT_STYLES[command]=$command_highlight_style
+    ZSH_HIGHLIGHT_STYLES[hashed-command]=$command_highlight_style
 
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=$quote_highlight_style
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=$quote_highlight_style
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=$quote_highlight_style
-
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=$option_highlight_style
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=$option_highlight_style
+    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=$quote_highlight_style
+    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=$quote_highlight_style
+    ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=$quote_highlight_style
+    
+    ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=$option_highlight_style
+    ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=$option_highlight_style
+fi
