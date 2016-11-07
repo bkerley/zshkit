dd7129575f2a74f6712a1a1ab23f18c6eee5da8a
Bryce Kerley <bkerley@brycekerley.net>
2016-11-07 14:39:05 -0500

    make sure the u+2604 COMET is text style and not emoji style

--- 06_status.zsh
+++ 06_status.zsh
@@ -2,6 +2,6 @@ function status-prompt() {
   if [[ $status = 0 ]]; then
       echo "%{$fg[green]%}\u2605%{$reset_color%}" # blame it on the black star
   else
-      echo "%{$fg[red]%}\u2604 $status%{$reset_color%}"   # blame it on the falling sky
+      echo "%{$fg[red]%}\u2604\ufe0e $status%{$reset_color%}"   # blame it on the falling sky
   fi
 }
