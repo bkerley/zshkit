4e10e83329cc5185cf452cc2182ba5cd26c80de9
Bryce Kerley <bkerley@brycekerley.net>
2016-12-03 13:23:29 -0500

    cargo path

--- 01_path.zsh
+++ 01_path.zsh
@@ -1,4 +1,4 @@
-path=($HOME/.rbenv/bin /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin /usr/local/bin $path /usr/local/mysql/bin /opt/git/bin /usr/texbin /usr/sbin /usr/local/share/npm/bin)
+path=($HOME/.rbenv/bin $HOME/.cargo/bin /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin /usr/local/bin $path /usr/local/mysql/bin /opt/git/bin /usr/texbin /usr/sbin /usr/local/share/npm/bin)
 
 # Allow MacPorts man pages
 manpath=($manpath /opt/local/man)
